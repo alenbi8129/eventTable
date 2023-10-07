01194226f85e2f3de7c029373735fb5fc21caab7
Alenbiju2003 <alenbi8129@gmail.com>
2023-10-07 16:02:06 +0530

    Explanatory Comments

--- src/EventTableCalculator.java
+++ src/EventTableCalculator.java
@@ -4,20 +4,15 @@ public class EventTableCalculator {
     public static void main(String[] args) {
         // Create a Scanner object to read input from the console
         Scanner scanner = new Scanner(System.in);
-
         // Get the group sizes from the user
         int[] groupSizes = getInputData(scanner);
-
         // Calculate the total number of guests
         int totalGuests = calculateTotalGuests(groupSizes);
-
         // Display the total number of guests
         System.out.println("The total number of guests is: " + totalGuests);
-
         // Close the scanner to release resources
         scanner.close();
     }
-
     // Function to get input data from the user
     public static int[] getInputData(Scanner scanner) {
         System.out.print("Enter the number of groups: ");
@@ -30,19 +25,15 @@ public class EventTableCalculator {
             System.out.print("Enter the size of group " + (i + 1) + ": ");
             groupSizes[i] = scanner.nextInt();
         }
-
         return groupSizes;
     }
-
     // Function to calculate the total number of guests
     public static int calculateTotalGuests(int[] groupSizes) {
         int totalGuests = 0;
-
         // Sum up the sizes of all groups to calculate the total
         for (int groupSize : groupSizes) {
             totalGuests += groupSize;
         }
-
         return totalGuests;
     }
 }
